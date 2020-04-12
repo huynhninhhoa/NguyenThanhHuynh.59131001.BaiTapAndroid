c071ab00d6937dad6e06b49989a01ec96b4ef1f3
Nguyễn Thành Huynh NTU <58835546+huynhninhhoa@users.noreply.github.com>
2020-04-12 21:23:22 +0700

    Add files via upload

---
+++ nguyenthanhhuynh59131001helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.nguyenthanhhuynh59131001helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
